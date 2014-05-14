914f559b0eae861187368ae9ecaf321dab1c3743
MikhailBobrutskov <mbobrutskov@lokata.ru>
2014-05-15 00:11:42 +0400

    improve thread pool

--- src/java/com/wizzardo/epoll/threadpool/ThreadPool.java
+++ src/java/com/wizzardo/epoll/threadpool/ThreadPool.java
@@ -10,13 +10,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ThreadPool {
 
     private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+    private int threadsCount;
+    private String threadNamePrefix;
 
     public ThreadPool(int threads) {
         this("ThreadPoolWorker", threads);
     }
 
     public ThreadPool(String name, int threads) {
-        for (int i = 0; i < threads; i++) {
+        threadsCount = threads;
+        threadNamePrefix = name;
+        startThreads(threads, 0);
+    }
+
+    private void startThreads(int count, int offset) {
+        for (int i = 0; i < count; i++) {
             Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -30,13 +38,20 @@ public class ThreadPool {
                 }
             });
             t.setDaemon(true);
-            t.setName(name + "-" + i);
+            t.setName(threadNamePrefix + "-" + (i + offset));
             t.start();
         }
     }
 
     public void add(Runnable r) {
         queue.add(r);
+    }
+
+    public void increaseThreadsCountTo(int newThreadsCount) {
+        if (newThreadsCount < threadsCount)
+            throw new IllegalArgumentException("new threads count cannot be smaller than old value: " + newThreadsCount + " < " + threadsCount);
+        startThreads(newThreadsCount - threadsCount, threadsCount);
+        threadsCount = newThreadsCount;
     }
 
 }
