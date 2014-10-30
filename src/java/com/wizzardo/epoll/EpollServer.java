995351a53fecadb2c27bbea9b45ec298460b439d
wizzardo <wizzardobox@gmail.com>
2014-10-30 22:07:33 +0300

    add host and port getters

--- src/java/com/wizzardo/epoll/EpollServer.java
+++ src/java/com/wizzardo/epoll/EpollServer.java
@@ -6,6 +6,9 @@ package com.wizzardo.epoll;
  */
 public class EpollServer<T extends Connection> extends EpollCore<T> {
 
+    private String host;
+    private int port;
+
     public EpollServer(int port) {
         this(null, port);
     }
@@ -17,5 +20,16 @@ public class EpollServer<T extends Connection> extends EpollCore<T> {
     public EpollServer(String host, int port, int maxEvents) {
         super(maxEvents);
         bind(host, port);
+
+        this.host = host == null ? "0.0.0.0" : host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
