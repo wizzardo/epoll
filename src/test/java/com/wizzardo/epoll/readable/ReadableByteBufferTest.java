782435ceab7bf4d66edc61b88f88cadef7e5c530
wizzardo <wizzardobox@gmail.com>
2016-04-25 22:45:24 +0200

    add tests for reading into another buffer

--- src/test/java/com/wizzardo/epoll/readable/ReadableByteBufferTest.java
+++ src/test/java/com/wizzardo/epoll/readable/ReadableByteBufferTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class ReadableByteBufferTest {
 
     @Test
-    public void test_1() {
+    public void test_sameBuffer() {
         ByteBuffer buffer = ByteBuffer.allocateDirect(256);
         ReadableByteBuffer readable = new ReadableByteBuffer(buffer);
 
@@ -21,10 +21,43 @@ public class ReadableByteBufferTest {
     }
 
     @Test
-    public void test_2() {
+    public void test_readAll() {
         ByteBuffer buffer = ByteBuffer.allocateDirect(256);
         ReadableByteBuffer readable = new ReadableByteBuffer(buffer);
         assertEquals(256, readable.read(readable.getByteBuffer(null).buffer()));
     }
 
+    @Test
+    public void test_read_into_heap_bb() {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(256);
+        for (int i = 0; i < 256; i++) {
+            buffer.put((byte) i);
+        }
+        ReadableByteBuffer readable = new ReadableByteBuffer(buffer);
+
+        ByteBuffer b = ByteBuffer.allocate(256);
+        assertEquals(256, readable.read(b));
+        assertEquals(256, b.position());
+        b.flip();
+        for (int i = 0; i < 256; i++) {
+            assertEquals((byte) i, b.get());
+        }
+    }
+
+    @Test
+    public void test_read_into_direct_bb() {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(256);
+        for (int i = 0; i < 256; i++) {
+            buffer.put((byte) i);
+        }
+        ReadableByteBuffer readable = new ReadableByteBuffer(buffer);
+
+        ByteBuffer b = ByteBuffer.allocateDirect(256);
+        assertEquals(256, readable.read(b));
+        assertEquals(256, b.position());
+        b.flip();
+        for (int i = 0; i < 256; i++) {
+            assertEquals((byte) i, b.get());
+        }
+    }
 }
